5f1353e5c0f856ad9261877683fba681d578ad85
Øystein Seel <oystein.seel@tradesolution.no>
2018-02-25 15:38:51 +0100

    some quick fixes and db reference changes

--- api/Controllers/TestController.cs
+++ api/Controllers/TestController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api.Models;
 
 namespace api.Controllers
 {
-    //[Route("api/[controller]")]
+    [Route("api/[controller]")]
     public class TestController : Controller
     {
          public IActionResult Index()  

f5bfc6a82513352f6b63b17d460daa32c249073f
seel93 <seel1993@gmail.com>
2018-03-04 19:08:06 +0100

    fix init file for sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,10 @@
-Create database testDb;
+create DATABASE testDb;
 Use testDb;
 
-create table Test(id int not null, name varchar(30) not null, email varchar(15) not null, phone int null);
+create table Test1(id int not null, name varchar(30) not null, email varchar(15) not null, phone int null);
+create table Test2(id int not null, name varchar(30) not null, email varchar(15) not null, phone int null);
+create table Test3(id int not null, name varchar(30) not null, email varchar(15) not null, phone int null);
 
-insert into Test(id, name, email, phone) values(1, "mail@mail.com", 78978945); 
+insert into Test1(id, name, email, phone) values(1, "stein", "mail@mail.com", 78978945); 
+insert into Test2(id, name, email, phone) values(1, "stein", "mail@mail.com", 78978945); 
+insert into Test3(id, name, email, phone) values(1, "stein", "mail@mail.com", 78978945); 

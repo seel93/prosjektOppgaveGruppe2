0a2d594a83b1a60b4ef888013c6e08506f2a890d
seel93 <seel1993@gmail.com>
2018-04-02 21:10:31 +0200

    init setup for orders

--- UI/src/app/equipment/equipment.component.ts
+++ UI/src/app/equipment/equipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-equipment',
@@ -10,6 +11,11 @@ export class EquipmentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(f: NgForm) { // html-form som tar i mot brukernavn og passord
+    console.log(f.value);
+    console.log(f.valid);
   }
 
 }

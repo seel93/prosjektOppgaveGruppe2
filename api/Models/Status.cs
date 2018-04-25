28853e88a075a7c92fea11c40ea4579c30de12f9
MaksimPas <super-max-007@yandex.ru>
2018-04-25 18:51:47 +0300

    deleted status models and controllers

--- api/Models/Status.cs
+++
@@ -1,9 +0,0 @@
-namespace  api.Models
-{
-    // status-tabellen
-    public class Status
-    {
-        public int Status_id {get; set;}
-        public string Current_Status {get; set;}
-    }
-}

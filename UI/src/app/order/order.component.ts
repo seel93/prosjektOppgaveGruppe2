941567ed271e49a0d279693441f738511d594b35
seel93 <seel1993@gmail.com>
2018-04-28 20:10:33 +0200

    more logic for order, fix fields for db tables

--- UI/src/app/order/order.component.ts
+++ UI/src/app/order/order.component.ts
@@ -6,14 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
-  public radioModel : string = "1";
-
-  addPerson(numOfPeople){
-    this.radioModel = numOfPeople.toString();
-  }
+  public personModel : string = "1";
+  public daysModel : string = "1";
+  public hourModel : string = "1";
+  public groups: boolean;
+  public hours: boolean; 
+  public days: boolean;
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
+  
+  addPerson(numOfPeople){
+    this.personModel = numOfPeople.toString();
+  }
+
+  addDays(numOfDays){
+    this.daysModel = numOfDays.toString();
+  }
+
+  addHour(numOfHours){
+    this.hourModel = numOfHours.toString();
+  }
+
+  daysOrHours(choice){
+    if(choice == 'hours'){
+      this.hours = true;; 
+      this.days = false; 
+    }else{
+      this.hours = false; 
+      this.days = true;
+    }
+  }
+
+  multiplePeople(choice){
+    if(choice == 'yes'){
+      this.groups = true;
+    }else{
+      this.groups = false;
+    }
+  }
+
 
 }

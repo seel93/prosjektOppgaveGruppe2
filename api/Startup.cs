ee4c71da976814ae250feed602e06b6438eba23a
seel93 <seel1993@gmail.com>
2018-04-30 10:28:58 +0200

    add context services to startup

--- api/Startup.cs
+++ api/Startup.cs
@@ -25,43 +25,15 @@ namespace api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Add(new ServiceDescriptor(typeof(BikeContext), new BikeContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(CredContext), new CredContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(CustomerContext), new CustomerContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(DeliveryContext), new DeliveryContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(EmployeeContext), new EmployeeContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(OrderContext), new OrderContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(PlaceContext), new PlaceContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(PostalPlaceContext), new PostalPlaceContext(Configuration.GetConnectionString("DefaultConnection"))));
             services.Add(new ServiceDescriptor(typeof(TestContext), new TestContext(Configuration.GetConnectionString("DefaultConnection"))));
-       /*      services.AddIdentity<ApplicationUser, IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-            // Password settings
-            options.Password.RequireDigit = true;
-                    options.Password.RequiredLength = 8;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = true;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequiredUniqueChars = 6;
-
-            // Lockout settings
-            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
-                    options.Lockout.MaxFailedAccessAttempts = 10;
-                    options.Lockout.AllowedForNewUsers = true;
-
-            // User settings
-            options.User.RequireUniqueEmail = true;
-                });
-
-                services.ConfigureApplicationCookie(options =>
-                {
-            // Cookie settings
-            options.Cookie.HttpOnly = true;
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-            // If the LoginPath isn't set, ASP.NET Core defaults 
-            // the path to /Account/Login.
-            options.LoginPath = "/Account/Login";
-            // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
-            // the path to /Account/AccessDenied.
-            options.AccessDeniedPath = "/Account/AccessDenied";
-                    options.SlidingExpiration = true;
-            }); */
             services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
             services.AddMvc();
         }

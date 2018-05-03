6efba7d31c1e7c301eb932f6a973212e8dd760a4
FelixTHG <felix.grimsrud@hotmail.com>
2018-05-03 15:51:15 +0200

    lagt til 4 flere bestillinger, og samsvarende rader i utstyr_has_bestilling

--- db/dummyData.sql
+++ db/dummyData.sql
@@ -39,6 +39,10 @@ insert into ansatt(ansatt_id, Jobb, fulltid, loenn, ansatt_password, steder_sted
 insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(1, 798.99, 0, 1, 1, '2013-12-13', '2013-12-13', '2013-12-14');
 insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(2, 179.99, 0, 3, 4, '2013-12-13 15:47:35', '2013-12-13 16:00:00', '2013-12-14 17:00:00');
 insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(3, 3397.96, 1, 5, 4, '2015-04-17 12:53:12', '2015-04-19 07:00:00', '2015-04-21 07:00:00');
+insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(4, 447, 0, 2, 1, '2017-07-13 13:50:37', '2017-07-13 13:50:37', '2017-07-13 16:50:37');
+insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(5, 3397.96, 1, 5, 4, '2016-08-20 15:53:23', '2016-08-23 13:00:00', '2016-08-25 13:00:00');
+insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(6, 4194, 0, 4, 1, '2017-11-27 21:42:52', '2017-11-29 07:00:00', '2017-12-05 07:00:00');
+insert into bestilling(bestilling_id, pris, gruppe, kunde_kunde_id, ansatt_ansatt_id, bestillingsdato, kan_hentes, maa_leveres_foer) values(7, 5096.94, 1, 3, 1, '2018-01-17 18:17:03', '2018-01-28 07:00:00', '2018-01-31 07:00:00');
 
 #UTSTYR_BESTILLING_SAMMENINSERT
 insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(1, 1);
@@ -47,6 +51,14 @@ insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) va
 insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(1, 3);
 insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(2, 3);
 insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(4, 3);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(3, 4);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(1, 5);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(2, 5);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(4, 5);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(1, 6);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(1, 7);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(2, 7);
+insert into Utstyr_has_bestilling(Utstyr_utstyr_id, bestilling_bestilling_id) values(4, 7);
 
 #LEVERINGINSERT
 insert into Levering (bestilling_bestilling_id, leveringsdato, leveringssted) values(1, '2013-12-13 19:34:54', 11);

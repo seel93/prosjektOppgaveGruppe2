7f751028cfc79816a82fe4d86d1ede373f9e0c33
FelixTHG <felix.grimsrud@hotmail.com>
2018-05-07 00:01:00 +0200

    la til en select bike order by status, og en select bestilling som bare finner en aktiv bestilling hvis den finnes.

--- api/Models/OrderContext.cs
+++ api/Models/OrderContext.cs
@@ -53,6 +53,41 @@ namespace api.Models
             return list;
         }
 
+
+        public List<Order> GetActiveOrder()
+        {
+            List<Order> list = new List<Order>();
+
+            using(MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                var command = "select * from bestilling where maa_leveres_foer >= NOW() AND kan_hentes <= NOW();";
+                cmd = new MySqlCommand(command, conn);
+                //cmd.ExecuteNonQuery();
+                
+                using(var reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        list.Add(new Order
+                        {
+                            Order_id = Convert.ToInt32(reader["bestilling_id"]),
+                            Price = reader["pris"].ToString(),
+                            IsGroupOrder = Convert.ToInt32(reader["gruppe"]),
+                            Customer_id = Convert.ToInt32(reader["Kunde_kunde_id"]),
+                            Employee_id = Convert.ToInt32(reader["ansatt_ansatt_id"]),
+                            OrderDate = (DateTime)reader["bestillingsdato"],
+                            IsAvailableFrom = (DateTime)reader["kan_hentes"],
+                            MustBeDeliveredBefore = (DateTime)reader["maa_leveres_foer"]
+                        });
+                    }
+                }
+                conn.Close();
+            }
+            return list;
+        }
+
         public void postOrder(Order Order)
         {
             if (Order == null)
